84e67b00e5bb9a942afd7eaa81aad0c1ff4dc14f
Gopal yadav <yadavgopal6675@gmail.com>
2024-07-11 23:20:17 +0530

    intro to string

---
+++ py_basic/strings/code.py
@@ -0,0 +1,21 @@
+#string in python
+
+name = "GOPAL"
+#access an element
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+#modify -->not possible becausee string is immutable that means you can't modify.
+# name[0] = 'T'
+# print(name)
+
+
+
+                 # String Slicing
+#print nickname 
+#starts from index 0  all the way till 2 (excluding)
+nickName = name[0:2]
+print(nickName)
