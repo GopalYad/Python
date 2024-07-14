aba52a0580bd3598ec922222eb665fc2e769a2fb
Gopal yadav <yadavgopal6675@gmail.com>
2024-07-14 21:42:19 +0530

    package

---
+++ py_basic/Dir/Dir.py
@@ -0,0 +1,11 @@
+# main.py
+
+from module1 import greet
+from module2 import add
+
+# Using functions from module1
+greet("Alice")
+
+# Using functions from module2
+result = add(3, 5)
+print("The result of addition is:", result)
