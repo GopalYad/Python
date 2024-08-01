cf946f77e637529d9de5587fe25e419717deb92c
Gopal yadav <yadavgopal6675@gmail.com>
2024-08-01 10:41:01 +0530

    Type Conversion

---
+++ py_basic/Variable&DataTypes/TypeCoversion.py
@@ -0,0 +1,11 @@
+birth_year = input('Birth year : ')
+print(type(birth_year))
+age = 2024 - int(birth_year)
+print(type(age))
+print(age)
+
+
+#Exercise : Ask a user thier weight (in pounds) , convert in to kilograms and print on terminal
+weight = input( 'Enter your weight(in pound)')
+pound_to_kilograms = float(weight)*0.4535924
+print( pound_to_kilograms)
